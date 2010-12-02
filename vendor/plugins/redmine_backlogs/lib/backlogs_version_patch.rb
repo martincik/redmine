e0742462f010cdc44e0d49fab69f7409b2e17f9d
Ladislav Martincik <ladislav.martincik@gmail.com>
2010-12-02 16:17:23 +0100

    Backlog plugin

---
+++ vendor/plugins/redmine_backlogs/lib/backlogs_version_patch.rb
@@ -0,0 +1,31 @@
+require_dependency 'version'
+
+module Backlogs
+  module VersionPatch
+    def self.included(base) # :nodoc:
+      base.extend(ClassMethods)
+      base.send(:include, InstanceMethods)
+    end
+  
+    module ClassMethods
+    end
+  
+    module InstanceMethods
+      def touch_burndown
+        BurndownDay.find(:all,
+          :joins => :version,
+          :conditions => ['burndown_days.version_id = ? and (burndown_days.created_at >= ? or burndown_days.created_at >= versions.effective_date)', self.id, Date.today]
+        ).each {|bdd|
+          BurndownDay.destroy(bdd.id)
+        }
+      end
+  
+      def burndown
+        return Sprint.find_by_id(self.id).burndown
+      end
+  
+    end
+  end
+end
+
+Version.send(:include, Backlogs::VersionPatch) unless Version.included_modules.include? Backlogs::VersionPatch
