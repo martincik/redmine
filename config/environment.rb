43019c4d50b358be56ed121c201d018ea726f9fc
Ladislav Martincik <ladislav.martincik@gmail.com>
2010-12-02 16:02:20 +0100

    Session store

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for sweepers
   config.load_paths += %W( #{RAILS_ROOT}/app/sweepers )
+  config.action_controller.session = { :key => "_redmine_session", :secret => "allstarsarehackersandhackersstarts" }
 
   # Force all environments to use the same logger level 
   # (by default production uses :info, the others :debug)
